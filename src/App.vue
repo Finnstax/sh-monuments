155faf72be2a98917af1e1071ceae6bac9686af9
Finnstax <finn.oschmann@stud.fh-luebeck.de>
2021-06-01 21:02:14 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">
+      SH Monuments Start 👋🏼
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+};
+</script>
