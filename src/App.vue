5c488f3ab62fd21920ef4fdaae23c1aaa501f0a5
Finnstax <finn.oschmann@stud.fh-luebeck.de>
2021-06-01 21:56:57 +0200

    Import monuments, first parse

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,253 @@
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ]
+  }
+  ```
+-->
 <template>
-  <div class="justify-center flex bg-yellow-300 items-center h-screen">
-    <div class="text-4xl">
-      SH Monuments Start 👋🏼
+  <div class="h-screen flex overflow-hidden bg-gray-100">
+    <TransitionRoot as="template" :show="sidebarOpen">
+      <Dialog
+        as="div"
+        static
+        class="fixed inset-0 flex z-40 md:hidden"
+        @close="sidebarOpen = false"
+        :open="sidebarOpen"
+      >
+        <TransitionChild
+          as="template"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75" />
+        </TransitionChild>
+        <TransitionChild
+          as="template"
+          enter="transition ease-in-out duration-300 transform"
+          enter-from="-translate-x-full"
+          enter-to="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leave-from="translate-x-0"
+          leave-to="-translate-x-full"
+        >
+          <div
+            class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800"
+          >
+            <TransitionChild
+              as="template"
+              enter="ease-in-out duration-300"
+              enter-from="opacity-0"
+              enter-to="opacity-100"
+              leave="ease-in-out duration-300"
+              leave-from="opacity-100"
+              leave-to="opacity-0"
+            >
+              <div class="absolute top-0 right-0 -mr-12 pt-2">
+                <button
+                  class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  @click="sidebarOpen = false"
+                >
+                  <span class="sr-only">Close sidebar</span>
+                  <XIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                </button>
+              </div>
+            </TransitionChild>
+            <div class="flex-shrink-0 flex items-center px-4">
+              <img
+                class="h-8 w-auto"
+                src="./assets/logo.png"
+                alt="SH Monuments"
+              />
+            </div>
+            <div class="mt-5 flex-1 h-0 overflow-y-auto">
+              <nav class="px-2 space-y-1">
+                <a
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.href"
+                  :class="[
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                  ]"
+                >
+                  <component
+                    :is="item.icon"
+                    :class="[
+                      item.current
+                        ? 'text-gray-300'
+                        : 'text-gray-400 group-hover:text-gray-300',
+                      'mr-4 flex-shrink-0 h-6 w-6',
+                    ]"
+                    aria-hidden="true"
+                  />
+                  {{ item.name }}
+                </a>
+              </nav>
+            </div>
+          </div>
+        </TransitionChild>
+        <div class="flex-shrink-0 w-14" aria-hidden="true">
+          <!-- Dummy element to force sidebar to shrink to fit close icon -->
+        </div>
+      </Dialog>
+    </TransitionRoot>
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden md:flex md:flex-shrink-0">
+      <div class="flex flex-col w-64">
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex flex-col h-0 flex-1">
+          <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
+            <img class="w-auto" src="./assets/logo.png" alt="SH Monuments" />
+          </div>
+          <div class="flex-1 flex flex-col overflow-y-auto">
+            <nav class="flex-1 px-2 py-4 bg-gray-800 space-y-1">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
+                ]"
+              >
+                <component
+                  :is="item.icon"
+                  :class="[
+                    item.current
+                      ? 'text-gray-300'
+                      : 'text-gray-400 group-hover:text-gray-300',
+                    'mr-3 flex-shrink-0 h-6 w-6',
+                  ]"
+                  aria-hidden="true"
+                />
+                {{ item.name }}
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col w-0 flex-1 overflow-hidden">
+      <div class="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
+        <button
+          class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+          @click="sidebarOpen = true"
+        >
+          <span class="sr-only">Open sidebar</span>
+          <MenuAlt2Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+        <div class="flex-1 px-4 flex justify-between">
+          <div class="flex-1 flex">
+            <form class="w-full flex md:ml-0" action="#" method="GET">
+              <label for="search_field" class="sr-only">Suche</label>
+              <div
+                class="relative w-full text-gray-400 focus-within:text-gray-600"
+              >
+                <div
+                  class="absolute inset-y-0 left-0 flex items-center pointer-events-none"
+                >
+                  <SearchIcon class="h-5 w-5" aria-hidden="true" />
+                </div>
+                <input
+                  id="search_field"
+                  class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                  placeholder="Suche"
+                  type="search"
+                  name="search"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <main class="flex-1 relative overflow-y-auto focus:outline-none">
+        <div class="py-6">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <h1 class="text-2xl font-semibold text-gray-900">
+              Denkmale in Schleswig-Holstein
+            </h1>
+          </div>
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <div class="py-4">
+              <MonumentList />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
+
 <script>
+import { ref } from "vue";
+import {
+  Dialog,
+  DialogOverlay,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import {
+  BellIcon,
+  HomeIcon,
+  MenuAlt2Icon,
+  UsersIcon,
+  XIcon,
+} from "@heroicons/vue/outline";
+import { SearchIcon } from "@heroicons/vue/solid";
+import MonumentList from "./components/MonumentList.vue";
+
+const navigation = [
+  { name: "Übersicht", href: "#", icon: HomeIcon, current: true },
+  { name: "Administration", href: "#", icon: UsersIcon, current: false },
+];
+
 export default {
-  name: 'App',
+  components: {
+    Dialog,
+    DialogOverlay,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    TransitionChild,
+    TransitionRoot,
+    BellIcon,
+    MenuAlt2Icon,
+    SearchIcon,
+    XIcon,
+    MonumentList
+  },
+  setup() {
+    const sidebarOpen = ref(false);
+
+    return {
+      navigation,
+      sidebarOpen,
+    };
+  },
 };
 </script>
---
+++ src/components/MonumentList.vue
@@ -0,0 +1,75 @@
+<template>
+  <ul
+    role="list"
+    class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+  >
+    <li
+      v-for="monument in monuments.slice(0, 100)"
+      :key="monument.Objektnummer"
+      class="relative"
+    >
+      <div
+        class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
+      >
+        <img
+          v-if="monument.FotoURL"
+          :src="monument.FotoURL"
+          alt=""
+          class="object-cover pointer-events-none group-hover:opacity-75"
+        />
+        <img
+          v-if="!monument.FotoURL"
+          src="../assets/noimagefound.png"
+          alt=""
+          class="object-cover pointer-events-none group-hover:opacity-75"
+        />
+        <button type="button" class="absolute inset-0 focus:outline-none">
+          <span class="sr-only"
+            >View details for {{ monument.Bezeichnung }}</span
+          >
+        </button>
+      </div>
+      <p
+        class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none"
+      >
+        {{ monument.Bezeichnung }}
+      </p>
+      <p
+        class="block text-sm font-medium text-gray-500 pointer-events-none truncate"
+      >
+        {{ monument["Adresse-Lage"] }}
+      </p>
+      <p class="block text-sm font-medium text-gray-500 pointer-events-none">
+        {{ monument.Kreis }}
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+const files = [
+  {
+    title: "IMG_4985.HEIC",
+    size: "3.9 MB",
+    source:
+      "https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+  },
+  // More files...
+];
+
+import monuments from "../data/denkmalliste.json";
+export default {
+  computed: {
+    monumentsWithImage() {
+      return monuments.filter((x) => x.FotoURL);
+    },
+  },
+  setup() {
+    console.log(monuments);
+    return {
+      files,
+      monuments,
+    };
+  },
+};
+</script>
